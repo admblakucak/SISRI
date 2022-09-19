36907f8d613cc6639eb804d09d5a4f4a14264307
admblakucak <adm.blakucak@gmail.com>
2022-09-20 01:12:43 +0700

    refresh

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -95,6 +95,6 @@ define('EVENT_PRIORITY_HIGH', 10);
 
 
 //Add script below to setup dynamic base url on Codeigniter 4
-$base = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']) : 'http://' . $_SERVER['HTTP_HOST'] . dirname($_SERVER['SCRIPT_NAME']);
+$base = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off' ? 'https://' . $_SERVER['HTTP_HOST'] : 'http://' . $_SERVER['HTTP_HOST'];
 
 defined('BASE') || define('BASE', $base);
