fdbc7c9751f6fd7ab911b7fea2ea7e1cc5c4c649
admblakucak <adm.blakucak@gmail.com>
2022-09-19 19:49:49 +0700

    migrate code 2

---
+++ app/Libraries/Access_API.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Libraries;
+
+class Access_API
+{
+    function post_authorize_dosen($email)
+    {
+        $option = [
+            'headers' => [
+                'x-secret-key' => '6YUYRMWt03DHW2oIcpac1Bm7ftWp8n6JQCbtwbbhVSfQThezDfybm4A1lTTDvzIe',
+            ],
+        ];
+        $client = \Config\Services::curlrequest($option);
+        $data = $client->request('POST', 'https://api.trunojoyo.ac.id:8212/siakad/v1/authorize', [
+            'form_params' => [
+                'email' => $email,
+            ],
+        ])->getBody();
+        return $data;
+    }
+    function get_data_api($url)
+    {
+        $option = [
+            'headers' => [
+                'x-secret-key' => '6YUYRMWt03DHW2oIcpac1Bm7ftWp8n6JQCbtwbbhVSfQThezDfybm4A1lTTDvzIe',
+            ],
+        ];
+        $client = \Config\Services::curlrequest($option);
+        $data = json_decode($client->get($url)->getBody())->data;
+        return $data;
+    }
+    function get_meta_api($url)
+    {
+        $option = [
+            'headers' => [
+                'x-secret-key' => '6YUYRMWt03DHW2oIcpac1Bm7ftWp8n6JQCbtwbbhVSfQThezDfybm4A1lTTDvzIe',
+            ],
+        ];
+        $client = \Config\Services::curlrequest($option);
+        $data = json_decode($client->get($url)->getBody())->meta;
+        return $data;
+    }
+}
