ec235da92fcc36de2667262391872f85c9b19915
MahrusSholeh <mahrus.xtkj@gmail.com>
2022-10-25 12:28:00 +0700

    Membuat tampilan profil dan edit profil

---
+++ app/Views/Profil/edit_profil.php
@@ -0,0 +1,81 @@
+<?php
+
+use CodeIgniter\Images\Image;
+?>
+<?= $this->extend('Template/content') ?>
+
+<?= $this->section('content') ?>
+
+<div class="container-fluid">
+    <div class="row mt-3">
+        <div class="col-xl-4">
+            <div class="card mb-4 mb-xl-0">
+                <div class="card-header">Edit Profil</div>
+                <div class="card-body text-center">
+                    <img class="img-account-profile rounded-circle mb-2" src="/assets/img/faces/6.jpg" alt="">
+                    <div class="row mt-3">
+                        <div class="col-12">
+                            <button class="btn btn-primary" type="button" data-bs-target="#modaladd" data-bs-toggle="modal">Upload Gambar</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal" id="modaladd">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content modal-content-demo">
+                            <div class="modal-header">
+                                <h6 class="modal-title">Upload Gambar</h6><button aria-label="Close" class="close" data-bs-dismiss="modal" type="button"><span aria-hidden="true">&times;</span></button>
+                            </div>
+                            <form action="<?php base_url() ?>" method="POST" enctype="multipart/form-data">
+                                <div class="modal-body">
+                                    <div class="form-group">
+                                        <label for="">Upload Gambar</label>
+                                        <div class="input-group file-browser">
+                                            <input type="text" class="form-control border-right-0 browse-file" placeholder="-" name="ket_berkas" readonly>
+                                            <label class="input-group-btn">
+                                                <span class="btn btn-default">
+                                                    Browse <input type="file" name="berkas" class="d-none" accept="image/*" multiple>
+                                                </span>
+                                            </label>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button class="btn ripple btn-primary" type="submit">Upload</button>
+                                    <button class="btn ripple btn-secondary" data-bs-dismiss="modal" type="button">Keluar</button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-xl-8">
+            <div class="card mb-4">
+                <div class="card-header">Detail Profil</div>
+                <div class="card-body">
+                    <form>
+                        <div class="mb-3">
+                            <label class="small" for="inputUsername">Nama</label>
+                            <input class="form-control" id="inputUsername" type="text" placeholder="Isikan Nama Anda">
+                        </div>
+                        <div class="mb-3">
+                            <label class="small" for="inputFirstName">Nim</label>
+                            <input class="form-control" id="inputFirstName" type="text" placeholder="Isikan Nim Anda">
+                        </div>
+                        <div class="mb-3">
+                            <label class="small mb-1" for="inputLastName">Prodi</label>
+                            <input class="form-control" id="inputLastName" type="text" placeholder="Isikan Prodi Anda">
+                        </div>
+                        <div class="mb-3">
+                            <label class="small mb-1" for="inputOrgName">Angkatan</label>
+                            <input class="form-control" id="inputOrgName" type="text" placeholder="Isikan Angkatan Anda">
+                        </div>
+                        <button class="btn btn-primary" type="button">Simpan</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection(); ?>
---
+++ app/Views/Profil/profil.php
@@ -0,0 +1,69 @@
+<?php
+
+use CodeIgniter\Images\Image;
+?>
+<?= $this->extend('Template/content') ?>
+
+<?= $this->section('content') ?>
+
+<div class="container-fluid">
+    <div class="row mt-3">
+        <div class="col-md-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <div class="d-flex flex-column align-items-center text-center">
+                        <img src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="Admin" class="rounded-circle" width="150">
+                        <div class="mt-3">
+                            <h4>Septian Dwi Hanggara</h4>
+                            <p class="text-secondary mb-1">170441100020</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-8">
+            <div class="card mb-3">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Nama</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            Septian Dwi Hannggara
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Nim</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            170441100020
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Prodi</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            Sistem Informasi
+                        </div>
+                    </div>
+                    <hr>
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h6 class="mb-0">Angkatan</h6>
+                        </div>
+                        <div class="col-sm-9 text-secondary">
+                            2017
+                        </div>
+                    </div>
+                    <hr>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection(); ?>
