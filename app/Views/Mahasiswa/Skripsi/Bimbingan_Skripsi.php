f02b7ee5d6eb4f747613e4c940589f77dc40a272
Sasmeka <verdysas@gmail.com>
2022-11-25 19:45:58 +0700

    Refresh

--- app/Views/Mahasiswa/Skripsi/Bimbingan_Skripsi.php
+++ app/Views/Mahasiswa/Skripsi/Bimbingan_Skripsi.php
@@ -11,7 +11,7 @@ use CodeIgniter\Images\Image;
     <div class="breadcrumb-header justify-content-between">
         <div class="my-auto">
             <div class="d-flex">
-                <h4 class="content-title mb-0 my-auto">Bimbingan</h4><span class="text-muted mt-1 tx-13 ms-2 mb-0">/ Proposal</span>
+                <h4 class="content-title mb-0 my-auto">Bimbingan</h4><span class="text-muted mt-1 tx-13 ms-2 mb-0">/ Skripsi</span>
             </div>
         </div>
     </div>
