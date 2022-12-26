e5954125fb31e1d8e0dff9914cdfb75ea0405dd4
Sasmeka <verdysas@gmail.com>
2022-12-27 01:34:36 +0700

    refresh

--- app/Views/template.php
+++ app/Views/template.php
@@ -35,7 +35,7 @@
     <table width="100%" style="border: 1px solid black;border-collapse: collapse;">
         <tr>
             <td style="border: 1px solid black;text-align:center;padding: 15px;" rowspan="2">
-                <img src="<?= FCPATH . 'image/Logo_UTM.png' ?>" style="width: 100px;">
+                <img src="<?= base_url('image/Logo_UTM.png') ?>" style="width: 100px;">
             </td>
             <td style="text-align:center;font-size: 18px;border: 1px solid black;"><b>FORM</b></td>
             <td style="border: 1px solid black;text-align:center;padding: 5px;" colspan="2">No. Dokumen : <b>F.S.JMF.05</b></td>
