a0823eeca16f7650b6efc5ca16fe2b35353ded15
Sasmeka <verdysas@gmail.com>
2022-12-28 12:03:52 +0700

    refresh

---
+++ app/Views/Kosong.php
@@ -0,0 +1,12 @@
+<?php
+
+use CodeIgniter\Images\Image;
+?>
+<?= $this->extend('Template/content') ?>
+
+<?= $this->section('content') ?>
+
+<div class="container-fluid">
+
+</div>
+<?= $this->endSection(); ?>
