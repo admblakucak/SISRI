bc7a404c451868faa34be7e0c64ae587c3bb8d08
Sasmeka <verdysas@gmail.com>
2022-12-28 12:25:38 +0700

    refresh

--- app/Views/Template/main-header.php
+++ app/Views/Template/main-header.php
@@ -37,8 +37,12 @@
 											</div>
 										</div>
 									</div>
-									<a class="dropdown-item" href="/profil"><i class="bx bx-user-circle"></i>Profil</a>
-									<a class="dropdown-item" href="/edit_profil"><i class="bx bx-cog"></i> Edit Profil</a>
+									<?php
+									if (session()->get('ses_id') != 'admin') {
+									?>
+										<a class="dropdown-item" href="/profil"><i class="bx bx-user-circle"></i>Profil</a>
+										<a class="dropdown-item" href="/edit_profil"><i class="bx bx-cog"></i> Edit Profil</a>
+									<?php } ?>
 									<a class="dropdown-item" href="/setting"><i class="bx bx-slider-alt"></i> Pengaturan Akun</a>
 									<a class="dropdown-item" href="/logout"><i class="bx bx-log-out"></i> Log Out</a>
 								</div>
