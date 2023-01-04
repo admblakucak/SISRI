6e6f4d19a4aa061cc870166dfe679d99a55f6186
Sasmeka <verdysas@gmail.com>
2023-01-04 17:48:12 +0700

    refresh

--- app/Views/Korprodi/daftar_nilai.php
+++ app/Views/Korprodi/daftar_nilai.php
@@ -28,11 +28,8 @@ use CodeIgniter\Images\Image;
                                             <th style="text-align: center; vertical-align: middle;"><span>Nim</span></th>
                                             <th style="text-align: center; vertical-align: middle;" class="col-sm-7 col-md-6 col-lg-2"><span>Nama</span></th>
                                             <th style="text-align: center; vertical-align: middle;" class="col-sm-7 col-md-6 col-lg-4"><span>Judul Skripsi</span></th>
-                                            <th style="text-align: center; vertical-align: middle;" class="col-sm-7 col-md-6 col-lg-3"><span>Nilai Dosen Pembimbing 1</span></th>
-                                            <th style="text-align: center; vertical-align: middle;" class="col-sm-7 col-md-6 col-lg-3"><span>Nilai Dosen Pembimbing 2</span></th>
-                                            <th style="text-align: center; vertical-align: middle;" class="col-sm-7 col-md-6 col-lg-3"><span>Nilai Dosen Penguji 1</span></th>
-                                            <th style="text-align: center; vertical-align: middle;" class="col-sm-7 col-md-6 col-lg-3"><span>Nilai Dosen Penguji 2</span></th>
-                                            <th style="text-align: center; vertical-align: middle;" class="col-sm-7 col-md-6 col-lg-3"><span>Nilai Dosen Penguji 3</span></th>
+                                            <th style="text-align: center; vertical-align: middle;" class="col-sm-7 col-md-6 col-lg-3"><span>Nilai Bimbingan</span></th>
+                                            <th style="text-align: center; vertical-align: middle;" class="col-sm-7 col-md-6 col-lg-3"><span>Nilai Ujian Skripsi</span></th>
                                             <th style=" text-align: center; vertical-align: middle;" class="col-sm-7 col-md-6 col-lg-4"><span>Nilai Akhir Angka</span></th>
                                             <th style=" text-align: center; vertical-align: middle;" class="col-sm-7 col-md-6 col-lg-4"><span>Nilai Akhir Huruf</span></th>
                                         </tr>
@@ -44,20 +41,70 @@ use CodeIgniter\Images\Image;
                                             $mhs = $db->query("SELECT * FROM tb_mahasiswa WHERE nim = '" . $key->id . "'")->getResult();
                                             $judul = $db->query("SELECT * FROM tb_pengajuan_topik WHERE nim = '" . $key->id . "'")->getResult();
                                             $nilai = $db->query("SELECT * FROM tb_mahasiswa WHERE nim = '" . $key->id . "'")->getResult();
-
+                                            $pembimbing1 = $db->query("SELECT * FROM tb_nilai WHERE nim = '" . $key->id . "' AND sebagai='pembimbing 1'")->getResult();
+                                            $pembimbing2 = $db->query("SELECT * FROM tb_nilai WHERE nim = '" . $key->id . "' AND sebagai='pembimbing 2'")->getResult();
+                                            $penguji1 = $db->query("SELECT * FROM tb_nilai WHERE nim = '" . $key->id . "' AND sebagai='penguji 1'")->getResult();
+                                            $penguji2 = $db->query("SELECT * FROM tb_nilai WHERE nim = '" . $key->id . "' AND sebagai='penguji 2'")->getResult();
+                                            $penguji3 = $db->query("SELECT * FROM tb_nilai WHERE nim = '" . $key->id . "' AND sebagai='penguji 3'")->getResult();
+                                            if (!empty($pembimbing1)) {
+                                                $nb_pembimbing1 = $pembimbing1[0]->nilai_bimbingan == NULL ? 0 : $pembimbing1[0]->nilai_bimbingan;
+                                                $ns_pembimbing1 = $pembimbing1[0]->nilai_ujian == NULL ? 0 : $pembimbing1[0]->nilai_ujian;
+                                            } else {
+                                                $nb_pembimbing1 = 0;
+                                                $ns_pembimbing1 = 0;
+                                            }
+                                            if (!empty($pembimbing2)) {
+                                                $nb_pembimbing2 = $pembimbing2[0]->nilai_bimbingan == NULL ? 0 : $pembimbing2[0]->nilai_bimbingan;
+                                                $ns_pembimbing2 = $pembimbing2[0]->nilai_ujian == NULL ? 0 : $pembimbing2[0]->nilai_ujian;
+                                            } else {
+                                                $nb_pembimbing2 = 0;
+                                                $ns_pembimbing2 = 0;
+                                            }
+                                            if (!empty($penguji1)) {
+                                                $ns_penguji1 = $penguji1[0]->nilai_ujian == NULL ? 0 : $penguji1[0]->nilai_ujian;
+                                            } else {
+                                                $ns_penguji1 = 0;
+                                            }
+                                            if (!empty($penguji2)) {
+                                                $ns_penguji2 = $penguji2[0]->nilai_ujian == NULL ? 0 : $penguji2[0]->nilai_ujian;
+                                            } else {
+                                                $ns_penguji2 = 0;
+                                            }
+                                            if (!empty($penguji3)) {
+                                                $ns_penguji3 = $penguji3[0]->nilai_ujian == NULL ? 0 : $penguji3[0]->nilai_ujian;
+                                            } else {
+                                                $ns_penguji3 = 0;
+                                            }
+                                            $nb = (($nb_pembimbing1 + $nb_pembimbing2) / 2) * (60 / 100);
+                                            $ns = (($ns_pembimbing1 + $ns_pembimbing2 + $ns_penguji1 + $ns_penguji2 + $ns_penguji3) / 5) * (40 / 100);
+                                            $total = $nb + $ns;
+                                            if ($total >= 80) {
+                                                $grade = "A";
+                                            } elseif ($total >= 75 && $total <= 79) {
+                                                $grade = "B+";
+                                            } elseif ($total >= 70 && $total <= 74) {
+                                                $grade = "B";
+                                            } elseif ($total >= 65 && $total <= 69) {
+                                                $grade = "C+";
+                                            } elseif ($total >= 60 && $total <= 64) {
+                                                $grade = "C";
+                                            } elseif ($total >= 55 && $total <= 59) {
+                                                $grade = "D+";
+                                            } elseif ($total >= 50 && $total <= 54) {
+                                                $grade = "D";
+                                            } else {
+                                                $grade = "E";
+                                            }
                                         ?>
                                             <tr>
                                                 <th scope="row"><?= $no ?></th>
                                                 <td><?= $key->id ?></td>
                                                 <td><?= $mhs[0]->nama ?></td>
                                                 <td><?= $judul[0]->judul_topik ?></td>
-                                                <td>80</td>
-                                                <td>75</td>
-                                                <td>85</td>
-                                                <td>90</td>
-                                                <td>95</td>
-                                                <td>100</td>
-                                                <td>A</td>
+                                                <td><?= $nb ?></td>
+                                                <td><?= $ns ?></td>
+                                                <td><?= $total ?></td>
+                                                <td><?= $grade ?></td>
                                             </tr>
                                         <?php $no++;
                                         } ?>
